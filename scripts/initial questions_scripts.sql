e8ac9ae70b2e9b1c7f86e6d1eaae0cc7db5bd7f9
Isabelle <isiybelle@gmail.com>
2025-02-13 15:44:59 -0500

    Post Review Commit

--- scripts/initial questions_scripts.sql
+++ scripts/initial questions_scripts.sql
@@ -7,16 +7,19 @@
 -- order by yearid desc nulls last;
 --1864-2017
 
+--Barry's query
+-- select min(year) as first_year, max(year) as last_year
+-- from homegames;
+
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
 -- select namelast, namefirst, height, g_all, name
 -- from people
 -- join appearances
 -- 	using(playerid)
 -- join teams
--- 	using(teamid)
+-- 	using(teamid, yearid)
 -- Where height = (select min(height)
--- 				from people)
--- group by namelast, namefirst, height, g_all, name;
+-- 				from people);
 -- Eddie Gaedel, 43 inches tall, 7975 games for St. Louis Browns.
 
 
@@ -28,10 +31,22 @@
 -- WHERE playerid = 'gaedeed01'
 -- LIMIT 1;
 
+--Barry's query
+-- select concat(namefirst, ' ', namelast), height, g_all, name
+-- from people
+-- join appearances
+-- 	using(playerid)
+-- join teams
+-- 	using(teamid, yearid)
+-- Where height = (select min(height)
+-- 				from people);
 
 
--- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
--- select schoolname, namelast, namefirst, sum(salary)::int::money as total_salary
+
+-- *3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
+-- select distinct schoolname, namelast, namefirst,
+-- 	-- sum(salary)::int::money as total_salary
+-- 	salary::numeric::money
 -- from collegeplaying
 -- join schools
 -- 	using(schoolid)
@@ -39,13 +54,41 @@
 -- 	using(playerid)
 -- join salaries
 -- 	using(playerid)
--- where schoolname = 'Vanderbilt University'
--- group by schoolname, namelast, namefirst
--- order by total_salary desc;
+-- -- where schoolname = 'Vanderbilt University'
+-- group by schoolname, namelast, namefirst, salary
+-- having schoolname = 'Vanderbilt University'
+-- order by namelast desc;
 -- David Price earned the most at $245,553,888.00
+-- "priceda01"
 
 
--- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+-- select *
+-- from people;
+
+-- select playerid, salary::numeric::money
+-- from salaries
+-- where playerid = 'priceda01';
+
+
+
+--Barry's query
+-- with vandy_players as (
+-- 	select distinct playerid, namefirst, namelast
+-- 	from collegeplaying
+-- 	join schools
+-- 		using(schoolid)
+-- 	join people
+-- 		using(playerid)
+-- 	where schoolid like 'vandy')
+-- select namefirst, namelast, sum(salary)::numeric::money as total_salary
+-- from vandy_players
+-- join salaries
+-- 	using(playerid)
+-- group by namefirst, namelast
+-- order by total_salary desc;
+
+
+-- -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
 -- select
 -- 	case
 -- 		when pos = 'OF' then 'Outfield'
@@ -69,6 +112,17 @@
 -- WHERE yearid >= 1920
 -- GROUP BY decade
 -- ORDER BY decade;
+
+
+--Barry's query
+-- with decades as (
+-- 	select concat((yearid/10 * 10)::text,'''s') as decade, *
+-- 	from teams
+-- 	where yearid >= 1920)
+-- select decade, round(sum(hr)/(sum(g)::numeric/2), 2) as avg_hr, round(sum(so)/(sum(g)::numeric/2),2) as avg_so
+-- from decades
+-- group by decade
+-- order by decade;
 
 --check
 -- select 
@@ -105,21 +159,24 @@
 
 
 -- 6. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases.
--- with total as(
--- 	select playerid, yearid, (sum(sb) + sum(cs)) as total_percent
--- 	from batting
--- 	where yearid = 2016
--- 	group by playerid, yearid
--- 	order by playerid)
--- select namelast, namefirst, batting.yearid, round((sum(sb) / sum(total_percent)),2) as percent_stolen
+-- select namelast, namefirst, round(sum(sb)::numeric / (sum(sb)::numeric + sum(cs)::numeric * 100),2) as percent_stolen
 -- from batting
--- join total
--- 	using(playerid)
 -- join people
 -- 	using(playerid)
--- where total_percent >= 20
--- 	and batting.yearid = 2016
--- group by namelast, namefirst, batting.yearid
+-- where batting.yearid = 2016
+-- group by namelast, namefirst
+-- having sum(sb) + sum(cs) >= 20
+-- order by percent_stolen desc;
+
+
+--Barry's query
+-- select namefirst, namelast, round(sum(sb)::numeric/(sum(sb)::numeric + sum(cs)::numeric * 100), 2) as percent_stolen
+-- from people
+-- join batting
+-- 	using(playerid)
+-- where yearid = 2016
+-- group by namefirst, namelast
+-- having sum(sb) + sum(cs) >= 20
 -- order by percent_stolen desc;
 
 
@@ -197,20 +254,36 @@
 
 --player strike 1981
 
--- 7b. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+-- *7b. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 -- with max_wins as (
 -- 	select yearid, max(w) as w
 -- 	from teams
 -- 	where yearid between 1970 and 2016
--- 		and yearid != 1981
+-- 		and yearid != 1981 and yearid != 1994
 -- 	group by yearid
 -- )
 -- select 
--- 	round(count(case when wswin = 'Y' then 'Keep' end)::numeric /
--- 	count(distinct yearid)::numeric, 2) as percent_max_wins_ws
+-- 	count(case when wswin = 'Y' then 'Keep' end)::numeric /
+-- 	count(yearid)::numeric as percent_max_wins_ws
 -- from teams
 -- join max_wins as mw
 -- 	using(yearid, w);
+
+
+--Barry's query
+-- with most_wins as (
+-- 	select yearid, max(w) as most_wins
+-- 	from teams
+-- 	where yearid between 1970 and 2016
+-- 		and yearid != 1981 and yearid != 1994
+-- 	group by yearid
+-- )
+-- select sum(case when wswin = 'Y' then 1 end) as total_ws_wins,
+-- round(avg(case when wswin = 'Y' then 1 else 0 end) * 100, 2) as win_pct
+-- from most_wins
+-- join teams
+-- using(yearid)
+-- where w = most_wins;
 
 
 -- select yearid, wswin, w
@@ -247,17 +320,30 @@
 -- order by wins;
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
--- select teams.name, park_name, sum(hg.attendance) / sum(hg.games)  as avg_attn
+-- select name, teams.park, sum(hg.attendance) / sum(hg.games)  as avg_attn
 -- from homegames as hg
 -- join teams
 -- 	on hg.team = teams.teamid
+-- 	and hg.year = teams.yearid
 -- join parks
 -- 	on hg.park = parks.park
 -- where year = 2016
 -- 	and (select sum(games)
 -- 		from homegames) >= 10
--- group by teams.name, park_name
+-- group by name, teams.park
 -- order by avg_attn desc
+-- limit 5;
+
+
+--barry's query
+-- select name, teams.park, homegames.attendance/games as avg_attendance
+-- from teams
+-- join homegames
+-- 	on team = teamid
+-- 	and year = yearid
+-- where yearid = 2016
+-- 	and games >= 10
+-- order by avg_attendance desc
 -- limit 5;
 
 -- select team, park, sum(hg.attendance) / sum(hg.games)  as avg_attn
@@ -341,6 +427,27 @@
 -- join teams
 -- 	using(teamid, yearid);
 
+
+--Barry's query
+-- select namefirst, namelast, name, yearid, awardid, awardsmanagers.lgid
+-- from awardsmanagers
+-- join managers
+-- 	using(playerid, yearid)
+-- join people
+-- 	using(playerid)
+-- join teams
+-- 	using(teamid, yearid)
+-- where playerid in (
+-- 	select playerid
+-- 	from awardsmanagers
+-- 	join managers
+-- 		using(playerid, yearid)
+-- 	where awardid like 'TSN%'
+-- 		and awardsmanagers.lgid in ('AL', 'NL')
+-- 	group by playerid
+-- 	having count(distinct awardsmanagers.lgid) = 2)
+-- 	and awardid like 'TSN%';
+
 --test
 -- select playerid, yearid, awardid
 -- from awardsmanagers
@@ -356,7 +463,7 @@
 -- WHERE awardid = 'TSN Manager of the Year' AND awardsmanagers.lgid = 'NL'
 -- GROUP BY namefirst, namelast, teamid, name, managershalf.yearid, awardid, awardsmanagers.lgid
 
--- 10 Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
+-- *10 Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 -- with homeruns as (
 -- 	select playerid, max(hr) as hr, yearid
 -- 	from batting
@@ -371,6 +478,21 @@
 -- where b.yearid = 2016
 -- 	and (left(coalesce(finalgame,2016::text),4)::numeric - left(debut,4)::numeric) >=10
 -- group by namefirst, namelast, hrs.hr
+-- order by hr desc;
+
+
+--Barry's query
+-- WITH most_hr AS
+-- (SELECT playerid, MAX(hr) as most_hr
+-- FROM batting
+-- GROUP BY playerid)
+-- select namefirst, namelast, hr
+-- from most_hr
+-- join batting
+-- using(playerid)
+-- join people
+-- using(playerid)
+-- where hr = most_hr and yearid = 2016 and left(debut, 4)::numeric <= 2007 and hr > 0
 -- order by hr desc;
 
 
@@ -456,6 +578,12 @@
 -- having teamid = 'PHI' or teamid = 'SFN' or teamid = 'COL'
 -- order by yearid;
 
+-- select yearid, teamid, w
+-- from teams
+-- where teamid = 'COL'
+-- 	and yearid >= 2000
+-- order by yearid;
+
 -- select teamid, yearid, sum(w) as wins
 -- from teams
 -- where yearid = 2000
@@ -519,8 +647,33 @@
 -- order by yearid desc;
 
 
+-- select teamid, yearid, attendance
+-- from teams
+-- order by yearid desc nulls last;
+
+
+--Zach's query
+-- WITH attendance_data AS (
+--     SELECT
+--         h.team,
+--         h.year,
+--         ROUND(SUM(h.attendance) / SUM(h.games), 0) AS avg_attendance
+--     FROM homegames h
+--     GROUP BY h.team, h.year)
+
+-- SELECT
+--     t.name AS team_name,
+--     ad.year,
+--     ad.avg_attendance,
+--     t.w AS total_wins
+-- FROM attendance_data ad
+-- JOIN teams t ON t.teamid = ad.team AND t.yearid = ad.year
+-- ORDER BY avg_attendance DESC;
+
 -- 13 It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. Investigate this claim and present evidence to either support or dispute this claim. First, determine just how rare left-handed pitchers are compared with right-handed pitchers. Are left-handed pitchers more likely to win the Cy Young Award? Are they more likely to make it into the hall of fame?
 
+
+--NOTED - Did an oopsie, calculated for batters instead of pitcher, but oh well! ¯\_(ツ)_/¯
 -- No, it doesn't seem as though being left handed or ambidexterous are decidedly more effective. About 34% of league players are left or both handed which is nearly equivalent to the number of Cy Young award winners. Comparatively, there is a slightly higher population in the Hall of fame at 40%, but that isn't an out of reach number. Overall, comparing these results to the population in the league, left handed or ambidexteriousness doesn't seem to result in unfair advantage or effectiveness.
 
 
@@ -574,3 +727,176 @@
 -- select *
 -- from halloffame
 -- where playerid = 'sislege01';
+
+
+
+--Emily's queries
+-- select count (throws)
+-- from people
+-- --number pitchers: 18135
+-- SELECT count(throws) AS rthrows,
+-- FROM people
+-- where throws = 'R'
+-- --14480
+-- --80 righty, 20 lefty
+-- --hall of fame
+-- select *
+-- from halloffame
+-- with lefty as
+--   (SELECT DISTINCT playerid
+--   FROM people
+--   inner join pitching
+--   using (playerid)
+--   where throws = 'L'
+--   )
+-- select distinct l.playerid
+-- from lefty as l
+-- inner join halloffame as h
+-- using (playerid)
+-- --141 rows for lefty hall of famer pitcher
+-- with righty as
+--   (SELECT DISTINCT playerid
+--   FROM people
+--   inner join pitching
+--   using (playerid)
+--   where throws = 'R'
+--   )
+-- select distinct r.playerid
+-- from righty as r
+-- inner join halloffame as h
+-- using (playerid)
+-- --347 rows for righty hall of famer pitchers
+-- --double checking with number of pitchers in hall of fame: 489 rather than expected 488--are there any ambidextrous pitchers????
+-- --YES THERE IS ONE AND THEIR NAME IS PAT VENDITTE: he threw switch!!!!
+-- --okay this makes sense now and math can proceed
+-- with pitchers as
+--   (SELECT DISTINCT playerid
+--   FROM people
+--   inner join pitching
+--   using (playerid)
+--   )
+-- select distinct r.playerid
+-- from pitchers as r
+-- inner join halloffame as h
+-- using (playerid)
+-- SELECT distinct playerid, throws, namefirst, namelast
+-- from people
+-- inner join pitching as r
+-- using (playerid)
+-- where throws NOT LIKE 'L' AND throws NOT LIKE 'R'
+-- --better way to check values
+-- SELECT count(distinct throws)
+-- from people
+-- --MATH SECTION
+-- --(141/488)*100 = 29% of hall of fame pitchers are lefty
+-- --cy young
+-- select *
+-- from awardsplayers
+-- where awardid ilike 'Cy%'
+-- order by awardid
+-- --total of 112 rows
+-- select count(distinct awardid)
+-- from awardsplayers
+-- where awardid ilike 'Cy%'
+-- group by awardid
+-- --1, helpfully
+-- --how many
+-- select count(distinct playerid)
+-- from awardsplayers
+-- where awardid ilike 'Cy%'
+-- --total of 77 distinct players
+-- --righty count of cy awardees: 53
+-- with righty as
+--   (SELECT DISTINCT playerid
+--   FROM people
+--   inner join pitching
+--   using (playerid)
+--   where throws = 'R'
+--   )
+-- select count(distinct r.playerid)
+-- from righty as r
+-- inner join awardsplayers
+-- using (playerid)
+-- where awardid ilike 'Cy%'
+-- --lefty count cy awardees: 24
+-- with lefty as
+--   (SELECT DISTINCT playerid
+--   FROM people
+--   inner join pitching
+--   using (playerid)
+--   where throws = 'L'
+--   )
+-- select count(distinct l.playerid)
+-- from lefty as l
+-- inner join awardsplayers
+-- using (playerid)
+-- where awardid ilike 'Cy%'
+-- --yay numbers align!!
+-- --math time: lefty percentage: (24/77)*100 = 31% lefty
+-- --leftys do out perform righty pitchers in awards. but how does that map to actual performance? TO BE CONTINUED
+
+
+
+--Ava's queries
+-- It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. Investigate this claim and present evidence to either support or dispute this claim.
+-- First, determine just how rare left-handed pitchers are compared with right-handed pitchers.
+-- SELECT COUNT(DISTINCT playerid)
+-- FROM people
+-- WHERE throws = 'L';
+-- SELECT COUNT(DISTINCT playerid)
+-- FROM people;
+-- -- 19.12% is the answer I want
+-- WITH lh AS (
+-- 	SELECT DISTINCT playerid
+-- 	FROM people
+-- 	WHERE throws = 'L'
+-- 	)
+-- SELECT ROUND((COUNT(lh.playerid)::numeric/COUNT(DISTINCT people.playerid)::numeric)*100, 2) AS percent_left_handed
+-- FROM people
+-- LEFT JOIN lh
+-- USING (playerid);
+-- -- Are left-handed pitchers more likely to win the Cy Young Award?
+-- SELECT COUNT(*)
+-- 	FROM people
+-- 	JOIN awardsplayers
+-- 	USING (playerid)
+-- 	WHERE throws = 'L' AND awardid = 'Cy Young Award';
+-- SELECT COUNT(*)
+-- 	FROM awardsplayers
+-- 	WHERE awardid = 'Cy Young Award';
+-- -- 33.04% is the answer I want
+-- WITH lh_cy AS (
+-- 	SELECT *
+-- 	FROM people
+-- 	JOIN awardsplayers
+-- 	USING (playerid)
+-- 	WHERE throws = 'L' AND awardid = 'Cy Young Award'
+-- 	)
+-- SELECT ROUND((COUNT(lh_cy)::numeric/COUNT(cy)::numeric)*100, 2) AS percent_leftie_winners
+-- FROM (SELECT *
+-- 	FROM awardsplayers
+-- 	WHERE awardid = 'Cy Young Award') AS cy
+-- LEFT JOIN lh_cy
+-- USING (playerid, yearid);
+-- -- About a third of all Cy Young Award wins are left-handed players. We know that only about 19% of players are left-handed, so I would say yes, left-handed players have a higher chance of winning the Cy Young Award.
+-- -- Are they more likely to make it into the hall of fame?
+-- SELECT COUNT(DISTINCT playerid)
+-- FROM halloffame
+-- JOIN people
+-- USING (playerid)
+-- WHERE throws = 'L';
+-- SELECT COUNT(DISTINCT playerid)
+-- FROM halloffame;
+-- -- 19.76% is the answer I'm looking for
+-- WITH lh_hof AS (
+-- 	SELECT *
+-- 	FROM halloffame
+-- 	LEFT JOIN people
+-- 	USING (playerid)
+-- 	WHERE throws = 'L'
+-- 	)
+-- SELECT ROUND((COUNT(DISTINCT lh_hof.playerid)::numeric/COUNT(DISTINCT hof.playerid)::numeric)*100, 2) AS percent_lefties_hof
+-- FROM halloffame AS hof
+-- LEFT JOIN lh_hof
+-- USING (playerid, yearid);
+-- -- No. There is no significant difference in the percentage of left-handed players overall and the percentage of left-handed players in the hall of fame.
